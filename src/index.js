0e6f54e6b38ec65d94c065825b58cc11469ce6d1
kenhardika <kenzamahardika@gmail.com>
2022-06-07 07:51:46 +0700

    update structure html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import './style.css';
 import saySomething from './printText.js'
+import { head } from 'lodash';
+
+function onLoadLayer() {
+    const header = document.createElement('div');
+    const content = document.createElement('div');
+    const footer = document.createElement('div');
+    const container = document.createElement('div');
+
+    header.className='header';
+    content.className='content';
+    footer.className='footer';
+    container.className='container';
+
+    header.textContent='this is the header';
+    content.textContent='this is the content';
+    footer.textContent='this is the footer';
+
+    container.append(header, content, footer);
+    document.body.append(container);
+}
+
 
 function callRestaurantName() {
     console.log('Welcome to The Rat-a-Toule');
     saySomething();
 }
 
-callRestaurantName();
+window.onload =()=> {
+    onLoadLayer();
+    callRestaurantName();
+}
