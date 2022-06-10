e36591be29adb62e3732e61d89db91e88f72317b
kenhardika <kenzamahardika@gmail.com>
2022-06-10 17:59:13 +0700

    add menu list

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,6 @@
 import animateAppear from ".";
+import foodCassoulet from '../components/img/food-Cassoulet.jpg';
+import foodFilet from '../components/img/food-filetmignon.jpg'
 
 function openMenu() {
     console.log('Open Menu');
@@ -17,9 +19,21 @@ function showMenu() {
     console.log('this is will show th emenu')
     const footer = document.querySelector('.footer');
     const menuLayer = document.createElement('div');
+    const menuOne = document.createElement('div');
+    const menuOnePic = document.createElement('img');
+    const menuOneCap = document.createElement('p');
 
+
+    menuOne.className='menuList';
+    menuOneCap.className= 'menuText';
+    menuOnePic.className='menuImg';
     menuLayer.className='menuLayer';
-    
+
+    menuOneCap.textContent ='Fillet Mignon with Buttery Souffle Cake Complementary';
+    menuOnePic.src=foodFilet;
+
+    menuOne.append(menuOnePic, menuOneCap);
+    menuLayer.append(menuOne);
     footer.append(menuLayer);
 }
 
