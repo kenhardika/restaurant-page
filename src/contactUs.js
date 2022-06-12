c04743a84c4a2aa830d7da4c4e88a2336d4309df
kenhardika <kenzamahardika@gmail.com>
2022-06-12 10:33:08 +0700

    change vid bg style
    more manageable, width in %.
    flex dir column now works with other

--- src/contactUs.js
+++ src/contactUs.js
@@ -1,8 +1,22 @@
+import { removeAllChildNodes } from "./menu";
 
-function openMenu() {
+function openContactUs() {
     console.log('openMenu');
 
     const main = document.querySelector('.main');
     removeAllChildNodes(main);
 
+    const title = document.createElement('div');
+        title.textContent='Contact Us';
+        title.className='titleContact';
+    main.append(title, createContactUs());
 }
+
+function createContactUs(){
+    const layer = document.createElement('div');
+    
+    layer.className='layerContact';
+
+}
+
+export default openContactUs
--- src/location.js
+++ src/location.js
@@ -15,16 +15,20 @@ function openLocation(){
 function createLocation() {
     const locationLayer = document.createElement('div');
     const iframeMaps = document.createElement('iframe');
+    const textLocation = document.createElement('p');
 
-    iframeMaps.className='locationMaps';
-    locationLayer.className='locationLayer';
+    iframeMaps.className ='locationMaps';
+    locationLayer.className ='locationLayer';
+    textLocation.className ='textLocation';
+
+    textLocation.textContent='60 Rue de Roquebillière, 06300 Nice, France'
 
     iframeMaps.src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4287.430115749213!2d7.28728590774934!3d43.7108015188692!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12cddbe15426d6e7%3A0x3b0782f73b249e3a!2sKFC%20Nice%20Riquier!5e0!3m2!1sen!2sid!4v1654993971435!5m2!1sen!2sid'
     iframeMaps.allowFullscreen='1';
     iframeMaps.loading='lazy';
     iframeMaps.referrerPolicy='no-referrer-when-downgrade';
 
-    locationLayer.append(iframeMaps);
+    locationLayer.append(iframeMaps, textLocation);
 
     return locationLayer
 }
