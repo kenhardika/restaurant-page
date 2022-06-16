a89a2b1e7ce0e7f936a4b8f5c97e55f734d715dd
kenhardika <kenzamahardika@gmail.com>
2022-06-16 18:13:01 +0700

    remove clean true

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    
   },
   optimization: {
     runtimeChunk: 'single',
